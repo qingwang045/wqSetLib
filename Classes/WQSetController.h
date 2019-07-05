d30bbe17d9f38933fc8e23ae685870580a69a4d2
WangQing <wangqing@WangQingdeiMac.local>
2019-07-05 17:36:00 +0800

    init

---
+++ Classes/WQSetController.h
@@ -0,0 +1,17 @@
+//
+//  WQSetController.h
+//  WQXitTest_Example
+//
+//  Created by WangQing on 2019/7/5.
+//  Copyright © 2019 qingwang045. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WQSetController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Classes/WQSetController.m
@@ -0,0 +1,37 @@
+//
+//  WQSetController.m
+//  WQXitTest_Example
+//
+//  Created by WangQing on 2019/7/5.
+//  Copyright © 2019 qingwang045. All rights reserved.
+//
+
+#import "WQSetController.h"
+
+@interface WQSetController ()
+
+@end
+
+@implementation WQSetController
+
+-(void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    NSLog(@"aaaaaa");
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
